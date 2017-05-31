8726f266e3fde145dea2544e392cfe442c0396e0
Joel Bakken and lincoln nguyen <joel@>
2017-05-31 09:23:45 -0700

    Add word + definition

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#javascript-showing").toggle();
+    $("#javascript-hidden").toggle();
+      });
+  });
+
+  $(document).ready(function() {
+    $(".clickable2").click(function() {
+      $("#operators-showing").toggle();
+      $("#operators-hidden").toggle();
+        });
+    });
