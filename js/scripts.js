a2520b7c27a9e1aadbddf4fa1c923ed3643c31e3
Joel Bakken and lincoln nguyen <lincoln@>
2017-05-31 10:08:33 -0700

    add boilerplate text to scripts file

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,124 @@ $(document).ready(function() {
   $(".clickable").click(function() {
     $("#javascript-showing").toggle();
     $("#javascript-hidden").toggle();
-      });
   });
+});
 
-  $(document).ready(function() {
-    $(".clickable2").click(function() {
-      $("#operators-showing").toggle();
-      $("#operators-hidden").toggle();
-        });
-    });
+$(document).ready(function() {
+  $(".clickable2").click(function() {
+    $("#operators-showing").toggle();
+    $("#operators-hidden").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $(".clickable3").click(function() {
+    $("#variables-showing").toggle();
+    $("#variables-hidden").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $(".clickable4").click(function() {
+    $("#-showing").toggle();
+    $("#-hidden").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $(".clickable5").click(function() {
+    $("#-showing").toggle();
+    $("#-hidden").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $(".clickable6").click(function() {
+    $("#variables-showing").toggle();
+    $("#variables-hidden").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $(".clickable7").click(function() {
+    $("#-showing").toggle();
+    $("#-hidden").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $(".clickable8").click(function() {
+    $("#-showing").toggle();
+    $("#-hidden").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $(".clickable9").click(function() {
+    $("#-showing").toggle();
+    $("#-hidden").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $(".clickable10").click(function() {
+    $("#-showing").toggle();
+    $("#-hidden").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $(".clickable11").click(function() {
+    $("#-showing").toggle();
+    $("#-hidden").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $(".clickable12").click(function() {
+    $("#-showing").toggle();
+    $("#-hidden").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $(".clickable13").click(function() {
+    $("#-showing").toggle();
+    $("#-hidden").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $(".clickable14").click(function() {
+    $("#-showing").toggle();
+    $("#-hidden").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $(".clickable15").click(function() {
+    $("#-showing").toggle();
+    $("#-hidden").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $(".clickable16").click(function() {
+    $("#-showing").toggle();
+    $("#-hidden").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $(".clickable17").click(function() {
+    $("#-showing").toggle();
+    $("#-hidden").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $(".clickable18").click(function() {
+    $("#-showing").toggle();
+    $("#-hidden").toggle();
+  });
+});
